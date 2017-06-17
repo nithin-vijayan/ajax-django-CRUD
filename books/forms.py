4e4e7c3d3636b71d73f4811a5858a929e9a9a3bc
nithin-vijayan <nithinkvijayan@icloud.com>
2017-06-18 00:06:48 +0530

    files cleared

--- books/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Book
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ('title', 'publication_date', 'author', 'price', 'pages', 'book_type', )
